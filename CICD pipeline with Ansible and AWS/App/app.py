f8c27ab088241a95c9574b4f4e4b9e5e15ea47c1
Malek <zaag.malek1@gmail.com>
2023-11-03 21:56:00 +0100

    .

--- CICD pipeline with Ansible and AWS/App/app.py
+++ CICD pipeline with Ansible and AWS/App/app.py
@@ -1,13 +1,38 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'  # Replace with your PostgreSQL connection details
+db = SQLAlchemy(app)
 
-app=Flask(__name__)
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    author = db.Column(db.String(255))
+    publication_date = db.Column(db.Date)
 
-@app.route("/")
-def hello():
-    return "hello world2"
+@app.route('/')
+def index():
+    books = Book.query.all()
+    return render_template('index.html', books=books)
 
-if __name__=="__main__":
+@app.route('/add_book', methods=['POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        publication_date = request.form['publication_date']
+        book = Book(title=title, author=author, publication_date=publication_date)
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+@app.route('/delete_book/<int:id>')
+def delete_book(id):
+    book = Book.query.get(id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
     app.run(debug=True, port=4000)
-    
-    
