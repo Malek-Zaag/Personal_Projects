68cd28957d071a3d677c3ee6525f989149625aa7
Malek <zaag.malek1@gmail.com>
2023-11-04 21:35:37 +0100

    adding_env_var

--- CICD pipeline with Ansible and AWS/Infra/Jenkins Server/main.tf
+++ CICD pipeline with Ansible and AWS/Infra/Jenkins Server/main.tf
@@ -11,8 +11,6 @@ provider "azurerm" {
   features {
 
   }
-  subscription_id = "cc7b3e92-7412-47e3-abe0-b6f5315d0d09"
-  tenant_id       = "91a00a7b-3710-4fe0-ab16-22c122307049"
 }
 
 resource "azurerm_resource_group" "rg" {
