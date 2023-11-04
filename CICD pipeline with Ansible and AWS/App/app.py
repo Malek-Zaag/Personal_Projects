5a39c7be9134b9bb0c1e017af09c80e4866f89f6
Malek <zaag.malek1@gmail.com>
2023-11-04 17:43:24 +0100

    writing

--- CICD pipeline with Ansible and AWS/App/app.py
+++ CICD pipeline with Ansible and AWS/App/app.py
@@ -20,7 +20,7 @@ def index():
     books = Book.query.all()
     return render_template('index.html', books=books)
 
-@app.route('/add_book', methods=['POST'])
+@app.route('/add_book', methods=['POST','GET'])
 def add_book():
     if request.method == 'POST':
         title = request.form['title']
@@ -30,6 +30,9 @@ def add_book():
         db.session.add(book)
         db.session.commit()
         return redirect(url_for('index'))
+    elif request.method =='GET':
+        return render_template('form.html')
+
 
 @app.route('/delete_book/<int:id>')
 def delete_book(id):
