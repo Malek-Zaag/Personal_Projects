eb97877ac5d7d838a5ffda43cad6d4c808ee1b0e
Malek <zaag.malek1@gmail.com>
2023-11-05 17:50:35 +0100

    finished_infra

--- CICD pipeline with Ansible and AWS/Infra/EC2/main.tf
+++ CICD pipeline with Ansible and AWS/Infra/EC2/main.tf
@@ -11,6 +11,95 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_vpc" "example" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "eu-central-1a"
+  tags = {
+    Name : "my-subnet"
+  }
+}
+
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+  tags = {
+    Name : "my-rtb"
+  }
+}
+
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name : "my-igw"
+  }
+}
+
+resource "aws_route_table_association" "rtb-assoc" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 23
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name : "instances security group"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami                         = "ami-0a485299eeb98b979"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subnet.id
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  count                       = 2
+  availability_zone           = "eu-central-1a"
+  associate_public_ip_address = true
+  key_name                    = "ec2-key-pair"
+
+  tags = {
+    Name : "ec2-${count.index}"
+  }
 }
