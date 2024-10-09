8e5604580f05326705a58876d2abc32ba4cf5466
Malek-Zaag <zaag.malek1@gmail.com>
2024-10-09 10:05:35 +0100

    feat: client can send msg

--- ChatRoom/src/Client.java
+++ ChatRoom/src/Client.java
@@ -9,32 +9,35 @@ public class Client implements Runnable {
     private Socket client;
     private PrintWriter out;
     private BufferedReader in;
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
     @Override
     public void run() {
         try {
             client = new Socket(InetAddress.getLocalHost(), 999);
             out = new PrintWriter(client.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            this.sendMessage();
-
-            out.println("hello server from client");
-        }catch (Exception e){
+            sendMessage();
+            chat();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    public void sendMessage(String ){
 
-    public void sendMessage(){
-        Scanner in=new Scanner(System.in);
+    }
+    public void chat() {
+        Scanner in = new Scanner(System.in);
         while (!client.isClosed()) {
-            String msg=in.nextLine();
+            String msg = in.nextLine();
             System.out.println(msg);
             out.write(msg);
+            out.write("\n");
             out.flush();
         }
-    }
-
-    public static void main(String[] args) {
-        Client client=new Client();
-        client.run();
     }
 }
--- ChatRoom/src/Server.java
+++ ChatRoom/src/Server.java
@@ -5,62 +5,70 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-public class Server implements Runnable {
-    private ArrayList<ConnectionHandler> connectionHandlers ;
-    private  ExecutorService pool;
+public class Server {
 
-    public Server(){
-        connectionHandlers= new ArrayList<>();
-        pool= Executors.newCachedThreadPool();
+    private BufferedReader in;
+
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
     }
-private BufferedReader in;
-    @Override
+
     public void run() {
-
-        try{
-            ServerSocket serverSocket=new ServerSocket(999);
-            Socket client= serverSocket.accept();
-            ConnectionHandler handler=new ConnectionHandler(client);
-            this.connectionHandlers.add(handler);
-            pool.execute(handler);
-
-        }catch (Exception e) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(999);
+            Socket client = serverSocket.accept();
+            ConnectionHandler handler = new ConnectionHandler(client);
+            Thread t = new Thread(handler);
+            t.start();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    static class ConnectionHandler implements Runnable{
+    static class ConnectionHandler implements Runnable {
+        private ArrayList<ConnectionHandler> connectionHandlers = new ArrayList<>();
         private Socket client;
         private PrintWriter out;
         private BufferedReader in;
 
         public ConnectionHandler(Socket client) {
-            this.client=client;
-        }
-        @Override
-        public void run(){
+            this.client = client;
+            System.out.println("A new Client is connected");
             try {
                 out = new PrintWriter(this.client.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
-                if (client.isConnected()){
-                    System.out.println("connected");
-                    in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                    String bf=in.readLine();
-                    System.out.println(bf);
-                    out.close();
-                    in.close();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                this.connectionHandlers.add(this);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-    }
 
-    public static void main(String[] args) {
-        Server server=new Server();
-        server.run();
+        @Override
+        public void run() {
+            String msg;
+            while (this.client.isConnected()) {
+                try {
+                    msg = in.readLine();
+                    System.out.println(msg);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        public void broadcastMsg(String msg) {
+            for (ConnectionHandler connectionHandler : connectionHandlers) {
+                try {
+                    connectionHandler.out.write(msg);
+                    connectionHandler.out.write("\n");
+                    connectionHandler.out.flush();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
